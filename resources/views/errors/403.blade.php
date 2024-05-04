ac7c825426cdf7819d274ea67df0cc949f19ea90
ktahmd <149021786+ktahmd@users.noreply.github.com>
2024-05-04 13:19:47 +0000

    new page error theme

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>403 - Forbidden</title>
+    <style>
+        body {
+            background-color: #58b85c; /* Green background */
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        div {
+            color: white; /* White text */
+            text-align: center;
+        }
+        h1 {
+            font-size: 4em; /* Adjust the font size as needed */
+        }
+    </style>
+</head>
+<body>
+    <div>
+    <h1>403 || Forbidden</h1>
+    <p>You are not authorized to access this page.</p>
+    </div>
+</body>
+</html>
