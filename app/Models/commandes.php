cfcd7b2390330ba7b6ec89a50300dfb4ab7f8229
ktahmd <149021786+ktahmd@users.noreply.github.com>
2024-05-06 11:08:34 +0000

    updated

--- app/Models/commandes.php
+++ app/Models/commandes.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Commande extends Model
+class commandes extends Model
 {
     use HasFactory;
 }
