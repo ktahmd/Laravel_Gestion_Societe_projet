cc0b952d9013afd8261bb86ecf1c042a5a7b25f3
ktahmd <149021786+ktahmd@users.noreply.github.com>
2024-05-06 18:49:23 +0000

    updated

--- database/migrations/2024_05_06_155503_create_factures_table.php
+++ database/migrations/2024_05_06_155503_create_factures_table.php
@@ -16,9 +16,10 @@ return new class extends Migration
             
             $table->integer('produit_id')->unsigned();
             $table->integer('commandes_id')->unsigned();
+            $table->integer('qty');
+            $table->integer('qtjjjy');
             $table->integer('client_id')->unsigned();
-            
-            
+
             $table->timestamps();
             
             $table->foreign('commandes_id')->references('id')->on('produit')->onDelete('cascade');
