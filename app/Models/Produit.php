27e8c8d6834c85557f1f148ec550d44982311c68
kneibatou <kneibanm@gmail.com>
2024-05-06 11:24:57 +0000

    model and migration

--- app/Models/Produit.php
+++ app/Models/Produit.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Produit extends Model
 {
     use HasFactory;
+    protected $table = 'produit';
+
+    protected $fillable = [
+        'nom',
+        'description',
+        'prix',
+        'categorie_id',
+        'quantite_stock',
+    ];
+
+    public function categorie()
+    {
+        return $this->belongsTo(Categorie::class,'categorie_id');
+    }
 }
